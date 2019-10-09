9b280ea3a8423445b6238e4483541ed6103c8b39
dayan1313 <56347871+dayan1313@users.noreply.github.com>
2019-10-09 18:20:51 +0600

    Add files via upload

---
+++ send.php
@@ -0,0 +1,43 @@
+﻿<?php
+$msg_box = ""; // в этой переменной будем хранить сообщения формы
+
+if($_POST['btn_submit']){
+$errors = array(); // контейнер для ошибок
+// проверяем корректность полей
+if($_POST['user_name'] == "") $errors[] = "Поле 'Ваше имя' не заполнено!";
+if($_POST['user_email'] == "") $errors[] = "Поле 'Ваш e-mail' не заполнено!";
+if($_POST['text_comment'] == "") $errors[] = "Поле 'Текст сообщения' не заполнено!";
+
+// если форма без ошибок
+if(empty($errors)){
+// собираем данные из формы
+$message = "Имя пользователя: " . $_POST['user_name'] . "<br/>";
+$message .= "E-mail пользователя: " . $_POST['user_email'] . "<br/>";
+$message .= "Текст письма: " . $_POST['text_comment'];
+send_mail($message); // отправим письмо
+// выведем сообщение об успехе
+$msg_box = "<span style='color: green;'>Сообщение успешно отправлено!</span>";
+}else{
+// если были ошибки, то выводим их
+$msg_box = "";
+foreach($errors as $one_error){
+$msg_box .= "<span style='color: red;'>$one_error</span><br/>";
+}
+}
+}
+// функция отправки письма
+function send_mail($message){
+// почта, на которую придет письмо
+$mail_to = "адрес_почты_получателя_";
+// тема письма
+$subject = "Письмо с обратной связи";
+
+// заголовок письма
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
+$headers .= "From: Тестовое письмо <адрес_почты_на_хосте>\r\n"; // от кого письмо
+
+// отправляем письмо
+mail($mail_to, $subject, $message, $headers);
+}
+?>
